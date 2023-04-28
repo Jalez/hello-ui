6fcb438d96da0af73ed5a8e673b374987b403343
Pia Niemela <pia.niemela@tuni.fi>
2023-04-28 15:21:20 +0300

    css-artist2

--- docker-up.sh
+++ docker-up.sh
@@ -1,5 +1,2 @@
-# this is run in tie-lukioplus.rd.tuni.fi
-# check /plussa/apache.conf
-# currently bound to the address tie-lukioplus.rd.tuni.fi/regex/
-docker build -t cssartist1 .
-docker run -d -p 54321:3000 --name cssartist1 --restart always cssartist1
+docker build -t cssartist2 .
+docker run -d -p 54322:3000 --name cssartist2 --restart always cssartist2
