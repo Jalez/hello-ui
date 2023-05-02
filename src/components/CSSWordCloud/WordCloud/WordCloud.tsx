334cfc761cf5be417aa2c2f52a6fd816cf1d1d7d
Jaakko Rajala <jaakko.rajala@tuni.fi>
2023-05-02 17:53:20 +0300

    Another fix attempt to imports.

--- src/components/CSSWordCloud/WordCloud/WordCloud.tsx
+++ src/components/CSSWordCloud/WordCloud/WordCloud.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 // import d3
-import * as d3Cloud from 'd3-cloud';
+import d3Cloud from 'd3-cloud';
 import * as d3 from 'd3';
 import './WordCloud.css';
 // const as = d3Cloud_ as any;
