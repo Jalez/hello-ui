2f65f2e97802682d07f3fed4d5b2459a40920b5f
Janne Miilunpalo <janne.miilunpalo@tuni.fi>
2024-03-15 13:27:21 +0200

    Remove unnecessary docker scripts from drawboard

--- drawBoard/docker-up.sh
+++
@@ -1,5 +0,0 @@
-# this is run in tie-lukioplus.rd.tuni.fi
-# check /plussa/apache.conf
-# currently bound to the address tie-lukioplus.rd.tuni.fi/drawboard2/
-docker build -t draw .
-docker run -d -p 54320:3000 --name drawboard --restart always draw
