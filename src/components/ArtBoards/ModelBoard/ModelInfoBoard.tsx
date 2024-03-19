69a4adcc8cf9ea67f5cac77db41f3b89ed15df7b
Jaakko Rajala <jaakko.rajala@tuni.fi>
2024-03-19 18:32:42 +0200

    Comment InfoSwitch from model info.

--- src/components/ArtBoards/ModelBoard/ModelInfoBoard.tsx
+++ src/components/ArtBoards/ModelBoard/ModelInfoBoard.tsx
@@ -1,27 +1,27 @@
 /** @format */
 
 // ModelInfoBoard.tsx
-import { InfoBoard } from '../../InfoBoard/InfoBoard';
-import { InfoColors } from '../../InfoBoard/InfoColors';
-import { InfoPictures } from '../../InfoBoard/InfoPictures';
-import { InfoSwitch } from '../../InfoBoard/InfoSwitch';
+import { InfoBoard } from "../../InfoBoard/InfoBoard";
+import { InfoColors } from "../../InfoBoard/InfoColors";
+import { InfoPictures } from "../../InfoBoard/InfoPictures";
+import { InfoSwitch } from "../../InfoBoard/InfoSwitch";
 
 type ModelInfoBoardProps = {
-	showModel: boolean;
-	setShowModel: (show: boolean) => void;
+  showModel: boolean;
+  setShowModel: (show: boolean) => void;
 };
 
 export const ModelInfoBoard = ({
-	showModel,
-	setShowModel,
+  showModel,
+  setShowModel,
 }: ModelInfoBoardProps) => (
-	<InfoBoard>
-		<InfoColors />
-		<InfoPictures />
-		<InfoSwitch
+  <InfoBoard>
+    <InfoColors />
+    <InfoPictures />
+    {/* <InfoSwitch
 			label={showModel ? 'Show model' : 'Show diff'}
 			checked={showModel}
 			switchHandler={() => setShowModel(!showModel)}
-		/>
-	</InfoBoard>
+		/> */}
+  </InfoBoard>
 );
