6a0ada3ec9dc710fb515d11e4d9edc04306d9cca
Jaakko Rajala <jaakko.rajala@tuni.fi>
2024-03-20 12:32:40 +0200

    Lock CSS:

--- src/components/Editors/Editors.tsx
+++ src/components/Editors/Editors.tsx
@@ -140,7 +140,7 @@ export const Editors = () => {
           levelIdentifier={identifier}
           template={cssCode}
           width={cssEditorWidth}
-          // locked={true}
+          locked={true}
         />
       </div>
     </div>
