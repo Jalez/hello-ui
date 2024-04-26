faf4bb7b326588c07438a7a64b6278e014c9939e
Janne Miilunpalo <janne.miilunpalo@tuni.fi>
2024-04-26 13:24:35 +0300

    Backend first draft (with new JSON format)

---
+++ editor/db/data.js
@@ -0,0 +1,155 @@
+const fs = require('fs');
+const path = require('path');
+
+const LEVELS_FILE = path.resolve(__dirname, '../levels.json');
+const LEVEL_MAPS_FILE = path.resolve(__dirname, '../maps.json');
+
+const readData = () => {
+  return JSON.parse(fs.readFileSync(LEVELS_FILE, 'utf8'));
+};
+
+const readLevelMaps = () => {
+  return JSON.parse(fs.readFileSync(LEVEL_MAPS_FILE, 'utf8'));
+};
+
+const writeData = data => {
+  fs.writeFileSync(LEVELS_FILE, JSON.stringify(data, null, 2), 'utf8');
+};
+
+const writeLevelMaps = data => {
+  fs.writeFileSync(LEVEL_MAPS_FILE, JSON.stringify(data, null, 2), 'utf8');
+};
+
+//  --------
+
+const getAllLevels = () => {
+  return readData();
+};
+
+const getAllMaps = () => {
+  return readLevelMaps();
+};
+
+const getLevelById = id => {
+  const levels = getAllLevels();
+  return id in levels ? levels[id] : null;
+};
+
+const getLevelsByMapName = mapName => {
+  const maps = getAllMaps();
+  const levelIds = maps[mapName]?.levels;
+  if (!levelIds) return null;
+
+  const levels = getAllLevels();
+  return levelIds.map(id => levels[id]);
+};
+
+const getLevelNames = () => {
+  const levels = getAllLevels();
+  return Object.keys(levels).map(id => ({
+    [id]: levels[id]['name']
+  }));
+};
+
+const getMapByName = name => {
+  const maps = getAllMaps();
+  return maps[name];
+};
+
+const getMapNames = () => {
+  const maps = getAllMaps();
+  return Object.keys(maps);
+};
+
+const addMap = (name, data) => {
+  const maps = getAllMaps();
+  const levels = getAllLevels();
+
+  // TODO: better error handling
+  if (!name || !data) return false;
+  if (name in maps) return false;
+  if (!data.levels || !Array.isArray(data.levels)) return false;
+  if (data.levels.some(level => !levels[level])) return false; // unknown level found
+
+  maps[name] = data;
+  writeLevelMaps({ ...maps });
+  return maps[name];
+};
+
+const addLevel = data => {
+  const levels = getAllLevels();
+  const id = data?.identifier; // FIXME: generate id if missing
+
+  // TODO: better error handling
+  if (!data) return false;
+  if (!id || id in levels) return false;
+
+  writeData({ ...levels, [id]: data });
+  return data;
+};
+
+const updateLevel = (id, data) => {
+  const levels = getAllLevels();
+
+  // TODO: better error handling
+  if (!id || !data) return false;
+  if (!levels[id]) return false;
+  if (data?.identifier !== id) return false;
+
+  levels[id] = { ...levels[id], ...data };
+  writeData({ ...levels });
+  return levels[id];
+};
+
+const updateMap = (name, data) => {
+  const maps = getAllMaps();
+
+  // TODO: better error handling
+  if (!name || !data) return false;
+  if (!maps[name]) return false;
+  if (data.levels && data.levels.some(level => !levels[level])) return false; // unknown level found
+
+  maps[name] = { ...maps[name], ...data };
+  writeLevelMaps({ ...maps });
+  return maps[name];
+};
+
+const deleteLevel = id => {
+  const levels = getAllLevels();
+
+  // TODO: better error handling
+  if (!id) return false;
+  if (!levels[id]) return false;
+
+  delete levels[id];
+  writeData({ ...levels });
+  return true;
+};
+
+const deleteMap = name => {
+  const maps = getAllMaps();
+
+  // TODO: better error handling
+  if (!name) return false;
+  if (!maps[name]) return false;
+
+  delete maps[name];
+  writeLevelMaps({ ...maps });
+  return true;
+};
+
+module.exports = {
+  addLevel,
+  addMap,
+  deleteLevel,
+  deleteMap,
+  getAllLevels,
+  getAllMaps,
+  getLevelById,
+  getLevelsByMapName,
+  getLevelNames,
+  getMapByName,
+  getMapNames,
+  updateLevel,
+  updateMap
+};
--- editor/server.js
+++ editor/server.js
@@ -1,13 +1,12 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const fs = require("fs");
-const cors = require("cors");
-const app = express();
-const PORT = 3000;
-const OpenAI = require("openai");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
 
-const openai = new OpenAI();
+const app = express();
+const router = require('./router.js');
+// const OpenAI = require('openai');
+
+const PORT = 3000;
+// const openai = new OpenAI();
 
 // TODO:
 // - Validation
@@ -17,241 +16,37 @@ const openai = new OpenAI();
 // - randomization: select maps randomly
 
 app.use(cors()); // This will enable CORS for all routes
-app.use(bodyParser.json());
+app.use(express.json());
+app.use('/', router);
 
-const LEVELS_FILE = "./levels.json";
-const LEVEL_MAPS_FILE = "./maps.json";
+// app.post('/chatGPT', async (req, res) => {
+//   const systemPrompt = req.body.systemPrompt;
+//   const prompt = req.body.prompt;
+//   try {
+//     const chatResponse = await openai.chat.completions.create({
+//       model: 'gpt-3.5-turbo-1106',
+//       response_format: { type: 'json_object' },
+//       messages: [
+//         {
+//           role: 'system',
+//           content: systemPrompt
+//         },
+//         {
+//           role: 'user',
+//           content: prompt
+//         }
+//       ]
+//     });
 
-function readData() {
-  return JSON.parse(fs.readFileSync(LEVELS_FILE, "utf8"));
-}
-
-function readLevelMaps() {
-  return JSON.parse(fs.readFileSync(LEVEL_MAPS_FILE, "utf8"));
-}
-
-function writeData(data) {
-  fs.writeFileSync(LEVELS_FILE, JSON.stringify(data, null, 2), "utf8");
-}
-
-function writeLevelMaps(data) {
-  fs.writeFileSync(LEVEL_MAPS_FILE, JSON.stringify(data, null, 2), "utf8");
-}
-
-app.get("/levels", (req, res) => {
-  res.json(readData());
-});
-
-app.get("/levels/:mapName", (req, res) => {
-  const mapName = req.params.mapName;
-  // if its "all", return all levels
-  const maps = readLevelMaps();
-  let data = readData();
-  if (mapName === "all") {
-    const levelNames = Object.keys(data);
-    const levelsData = levelNames.map((levelName) => data[levelName]);
-    return res.json(levelsData);
-  } else if (!maps[mapName]) {
-    return res.status(404).send({ message: "Map not found" });
-  } else {
-    const levelNames = maps[mapName].levels;
-    const levelsData = levelNames.map((levelName) => data[levelName]);
-  }
-  console.log("levelsData", levelsData[0].name);
-  res.json(levelsData);
-});
-
-//Get all level names
-app.get("/levels/names", async (req, res) => {
-  try {
-    const data = readData();
-    const levelNames = Object.keys(data);
-    res.json(levelNames);
-  } catch (error) {
-    res.status(500).send({ message: "Failed to fetch data", error });
-  }
-});
-
-app.get("/maps", (req, res) => {
-  try {
-    res.json(readLevelMaps());
-  } catch (error) {
-    res.status(500).send({ message: "Failed to fetch data", error });
-  }
-});
-
-app.get("/maps/names", async (req, res) => {
-  try {
-    const maps = readLevelMaps();
-    const mapNames = Object.keys(maps);
-    res.json(mapNames);
-  } catch (error) {
-    res.status(500).send({ message: "Failed to fetch data", error });
-  }
-});
-
-//Get all levels in a map
-app.get("/maps/:name", async (req, res) => {
-  try {
-    const mapName = req.params.name;
-    const maps = readLevelMaps();
-    if (!maps[mapName]) {
-      return res.status(404).send({ message: "Map not found" });
-    }
-    res.json(maps[mapName]);
-  } catch (error) {
-    res.status(500).send({ message: "Failed to fetch data", error });
-  }
-});
-
-app.put("/levels/:id", (req, res) => {
-  // FIXME: Implement properly
-  try {
-    console.log("POST");
-    console.log("req.body", req.body);
-    writeData(req.body);
-    res.send({ message: "Data updated successfully", data: req.body });
-  } catch (error) {
-    res.status(500).send({ message: "Failed to update data", error });
-  }
-});
-//Add an endpoint to update an existing maps randomization and canUseAI properties
-app.put("/maps/:name", (req, res) => {
-  try {
-    let currentData = readLevelMaps();
-    const mapName = req.params.name;
-    if (!currentData[mapName]) {
-      return res.status(404).send({ message: "Map not found" });
-    }
-    currentData[mapName] = { ...currentData[mapName], ...req.body };
-    writeLevelMaps(currentData);
-    res.send({
-      message: "Data updated successfully",
-      map: currentData[mapName],
-    });
-  } catch (error) {
-    res.status(500).send({ message: "Failed to update maps", error });
-  }
-});
-
-// Add new map
-app.post("/maps", (req, res) => {
-  try {
-    let currentData = readLevelMaps();
-    const mapName = req.body.name;
-    if (currentData[mapName]) {
-      return res.status(400).send({ message: "Map already exists" });
-    }
-    // Add new map with structure conforming to the new format
-    currentData[mapName] = {
-      levels: [],
-      canUseAI: false, // default false, can be overwritten by request body
-      random: 0, // default 0, can be overwritten by request body
-    };
-    writeLevelMaps(currentData);
-    res.send({
-      message: "New map added successfully",
-      map: currentData[mapName],
-    });
-  } catch (error) {
-    res.status(500).send({ message: "Failed to update maps", error });
-  }
-});
-
-//Add a new level to an existing map
-app.post("/maps/:mapName", (req, res) => {
-  try {
-    let currentData = readLevelMaps();
-    const mapName = req.params.mapName;
-    if (!currentData[mapName]) {
-      return res.status(404).send({ message: "Map not found" });
-    }
-    if (currentData[mapName].levels.includes(req.body.name)) {
-      return res.status(400).send({ message: "Level already exists" });
-    }
-    currentData[mapName].levels.push(req.body.name);
-    writeLevelMaps(currentData);
-    console.log("req.body", req.body);
-    res.send({
-      message: "Data updated successfully",
-      map: currentData[mapName],
-    });
-  } catch (error) {
-    res.status(500).send({ message: "Failed to update maps", error });
-  }
-});
-
-app.post("/levels", (req, res) => {
-  try {
-    let currentData = readData();
-    currentData = { ...currentData, ...req.body };
-    writeData(currentData);
-    res.json({ message: "Data modified successfully" }); // Ensure JSON response
-  } catch (error) {
-    res.status(500).send({ message: "Failed to modify data", error });
-  }
-});
-
-app.put("/maps", (req, res) => {
-  // FIXME: Validate data
-  try {
-    let currentData = readLevelMaps();
-    currentData = { ...currentData, ...req.body };
-    writeLevelMaps(currentData);
-    res.json({ message: "Data modified successfully", map: req.body }); // Ensure JSON response
-  } catch (error) {
-    res.status(500).send({ message: "Failed to modify maps", error });
-  }
-});
-
-app.delete("/levels/:id", (req, res) => {
-  // FIXME: level only one level at a time
-  try {
-    writeData({});
-    res.send({ message: "Data cleared successfully" });
-  } catch (error) {
-    res.status(500).send({ message: "Failed to clear data", error });
-  }
-});
-
-app.delete("/maps/:name", (req, res) => {
-  // FIXME: delete only one level at a time
-  try {
-    writeLevelMaps({});
-    res.send("Data cleared successfully");
-  } catch (error) {
-    res.status(500).send({ message: "Failed to clear data", error });
-  }
-});
-
-app.post("/chatGPT", async (req, res) => {
-  const systemPrompt = req.body.systemPrompt;
-  const prompt = req.body.prompt;
-  try {
-    const chatResponse = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-1106",
-      response_format: { type: "json_object" },
-      messages: [
-        {
-          role: "system",
-          content: systemPrompt,
-        },
-        {
-          role: "user",
-          content: prompt,
-        },
-      ],
-    });
-
-    // Assuming the model returns the correct JSON structure directly
-    console.log(chatResponse.choices[0].message.content);
-    res.json(chatResponse.choices[0].message.content);
-  } catch (error) {
-    res
-      .status(500)
-      .send({ message: "Failed to fetch response from OpenAI", error });
-  }
-});
+//     // Assuming the model returns the correct JSON structure directly
+//     console.log(chatResponse.choices[0].message.content);
+//     res.json(chatResponse.choices[0].message.content);
+//   } catch (error) {
+//     res
+//       .status(500)
+//       .send({ message: 'Failed to fetch response from OpenAI', error });
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
