e4d1a7318edf738bfd33f670f4f4d80a31c60c62
Janne Miilunpalo <janne.miilunpalo@tuni.fi>
2024-05-06 15:44:14 +0300

    Remove unnecessary old comments

--- docker-stop.sh
+++ docker-stop.sh
@@ -16,39 +16,3 @@ if [[ "$(hostname)" =~ tie-lukioplus.rd.tuni.fi ]]; then
 fi
 
 docker compose --file ${COMPOSE_YML} down
-
-
-# service="${1:-all}"
-
-# usage() {
-#   echo "Usage: ./docker-stop.sh [ui|drawboard|all]"
-#   exit 1
-# }
-
-# stop_ui() {
-#   docker stop cssartist && docker container rm cssartist
-# }
-
-# stop_drawboard() {
-#   docker stop drawboard && docker container rm drawboard
-# }
-
-# case "${service}" in
-#   ui|uidesigner|ui-designer|cssartist|css-artist)
-#     echo "Stopping ui..."
-#     stop_ui
-#     ;;
-#   board|draw|drawboard|drawBoard)
-#     echo "Stopping drawboard..."
-#     stop_drawboard
-#     ;;
-#   all)
-#     echo "Stopping ui and drawboard..."
-#     stop_ui
-#     stop_drawboard
-#     ;;
-#   *)
-#     usage
-#     ;;
-# esac
-
