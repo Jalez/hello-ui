d0277d067438ba2f650741597233356bec95b0e8
Jaakko Rajala <jaakko.rajala@tuni.fi>
2024-12-31 12:49:35 +0200

    Add modifications

--- chatGPT/controllers/chatGPT.js
+++ chatGPT/controllers/chatGPT.js
@@ -1,5 +1,9 @@
 const OpenAI = require("openai");
-const openai = new OpenAI();
+
+require('dotenv').config();
+// const OpenAI = require('openai');
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+// const openai = new OpenAI();
 
 const models = [
   {
