6389cbab1b6a796cfc7e4b03821cd77fa65260cc
Jaakko Rajala <jaakko.rajala@tuni.fi>
2026-03-03 14:37:49 +0200

    Set basepath

--- docker-up.sh
+++ docker-up.sh
@@ -24,6 +24,7 @@ if [[ "$(hostname)" =~ tie-lukioplus.rd.tuni.fi ]]; then
   docker build -t ui-designer-app:latest \
     --build-arg NEXT_PUBLIC_DRAWBOARD_URL=https://tie-lukioplus.rd.tuni.fi/drawboard \
     --build-arg NEXT_PUBLIC_ASSET_PREFIX=/css-artist \
+    --build-arg NEXT_PUBLIC_BASE_PATH=/css-artist \
     -f Dockerfile .
   docker build -t ui-designer-ws:latest -f ws-server/Dockerfile ./ws-server
   docker build -t ui-designer-db-init -f Dockerfile.db-init .
