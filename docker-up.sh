044ec2abeadea5adb25784b7571ed7896f632d26
Jaakko Rajala <jaakko.rajala@tuni.fi>
2026-03-06 19:16:29 +0200

    Update with new args

--- docker-up.sh
+++ docker-up.sh
@@ -23,7 +23,9 @@ if [[ "$(hostname)" =~ tie-lukioplus.rd.tuni.fi ]]; then
   # 1. Build all images first (bypasses docker-compose Unicode crash)
   echo "Building images..."
   docker build -t ui-designer-app:latest \
+    --build-arg NEXT_PUBLIC_APP_URL=https://tie-lukioplus.rd.tuni.fi/css-artist \
     --build-arg NEXT_PUBLIC_DRAWBOARD_URL=https://tie-lukioplus.rd.tuni.fi/drawboard \
+    --build-arg NEXT_PUBLIC_WEBSOCKET_URL=https://tie-lukioplus.rd.tuni.fi/css-artist/ws \
     --build-arg NEXT_PUBLIC_ASSET_PREFIX=/css-artist \
     --build-arg NEXT_PUBLIC_BASE_PATH=/css-artist \
     -f Dockerfile .
