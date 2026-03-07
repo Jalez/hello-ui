75165b8fd95d58a6678e25e4fecda1a8984ec140
Jaakko Rajala <jaakko.rajala@tuni.fi>
2026-03-07 23:49:47 +0200

    Fix view

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
   return (
     <div className="relative min-h-screen overflow-hidden">
-      <div className="absolute bottom-8 left-1/2 z-10 w-full max-w-2xl -translate-x-1/2 px-8 md:bottom-12">
+      <div className="absolute bottom-20 left-1/2 z-10 w-full max-w-2xl -translate-x-1/2 px-6 sm:px-8 md:bottom-24">
         <div className="flex flex-wrap gap-4 justify-center">
           {isLoading ? (
             <Button size="lg" disabled className="text-lg px-8 py-6">
